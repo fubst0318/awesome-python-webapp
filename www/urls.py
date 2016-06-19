fc653e04d80334107b2246cb3776400759ba4aca
fubst-cxj <xuzhan@baosight.com>
2016-06-19 10:07:31 +0800

    Localhost Finished

--- www/urls.py
+++ www/urls.py
@@ -1,17 +1,27 @@
-#urls.py
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 __author__ = 'Tony Xu'
 
-import logging
+import os, re, time, base64, hashlib, logging
 
-from transwrap.web import get, view
+from transwrap.web import get, post, ctx, view, interceptor, seeother, notfound
+
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 
 from models import User, Blog, Comment
 
-@view('test_users.html')
+@view('blogs.html')
 @get('/')
-def test_users():
-    users = User.find_all()
+def index():
+    blogs = Blog.find_all()
+    user = User.find_first('where email=?', 'admin@example.com')
+    return dict(blogs=blogs, user=user)
+
+@api
+@get('/api/users')
+def api_get_users():
+    users = User.find_by('order by created_at desc')
+    for u in users:
+        u.password = '******'
     return dict(users=users)
